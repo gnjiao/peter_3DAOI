281516baa306fa46436653033236881725a77ded
peter <535978547@qq.com>
2017-11-21 20:19:04 +0800

    修改内容:     测试自定义异常类     添加自定义异常类注释

--- src/3DAOI/SDK/customexception.hpp
+++ src/3DAOI/SDK/customexception.hpp
@@ -5,7 +5,7 @@
 #include <string>
 #include <sstream>
 #include <iostream>
-
+// 异常时,将文件名、行号、函数名和详细信息+上一层的异常一起抛出
 #define THROW_EXCEPTION(exMsg)\
 {\
      std::ostringstream message;\
@@ -19,20 +19,54 @@
 
 namespace SDK
 {
-
+/**
+ *  @brief CustomException
+ *
+ *  @author peter
+ *  @version 1.00 2017-11-21 peter
+ *                note:create it
+ */
     class CustomException : public std::exception
     {
+
     public:
+//>>>----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        // constructor & destructor
+        /**
+         * @brief CustomException
+         *      构造函数
+         * @param N/A
+         * @return N/A
+         */
         CustomException();
+        /**
+         * @brief CustomException
+         *      构造函数
+         * @param message
+         *      需要显示的异常信息
+         * @return N/A
+         */
         CustomException(std::string& message);
+        /**
+         * @brief ~CustomException
+         *      析构函数
+         * @param N/A
+         * @return N/A
+         */
         virtual ~CustomException();
 
+//<<<----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+//>>>----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
         const std::string originalMsg ( )const{return this->m_originalMsg;}
 
         virtual const char* what() const _GLIBCXX_USE_NOEXCEPT override
         {
             return m_originalMsg.data();
         }
+//<<<----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
     private:
         std::string m_originalMsg;
     };
--- src/3DAOI/main.cpp
+++ src/3DAOI/main.cpp
@@ -1,30 +1,11 @@
 #include <iostream>
 
-#include <QSettings>
-#include <QFile>
-
 #include "./SDK/customexception.hpp"
 
 using namespace std;
 
-void testEx()
-{
-    try
-    {
-        if( 1)
-        {
-            THROW_EXCEPTION("5>1\n");
-        }
-    }
-    catch(const SDK::CustomException& ex)
-    {
-        cout << ex.what()<<endl;
-    }
-}
-
 int main()
 {
-    testEx();
     cout << "Hello World!" << endl;
     return 0;
 }
