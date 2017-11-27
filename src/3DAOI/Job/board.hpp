900ad0cc7b615f06564b43b5229c024cc9718bdc
peter <535978547@qq.com>
2017-11-27 10:06:33 +0800

    修改内容：     修改region排版

--- src/3DAOI/Job/board.hpp
+++ src/3DAOI/Job/board.hpp
@@ -1,11 +1,30 @@
 #ifndef BOARD_HPP
 #define BOARD_HPP
 
+#include <iostream>
 
-class Board
+#include "measurelist.hpp"
+
+namespace Job
 {
-public:
-    Board();
-};
+    class Board
+    {
+    public:
+        Board();
+        virtual Board();
+
+        void writeToXml(std::string name);
+
+
+    private:
+        Job::MeasureList m_measureList;
+        std::string m_name;
+        double m_sizeX;
+        double m_sizeY;
+        double m_originalX;
+        double m_originalY;
+    };
+}
+
 
 #endif // BOARD_HPP
